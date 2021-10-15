0db7f4b097f5a943a142d814aa224d75c6654d92
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2021-10-15 20:44:50 +0700

    building the layout for 3.1 website

---
+++ 3.1-time-tracking-dashboard-main/script.js
@@ -0,0 +1,30 @@
+// WORK
+// 32hrs <!-- weekly -->
+// Previous - 36hrs <!-- weekly -->
+// 103hrs <!-- monthly -->
+// Previous - 128hrs <!-- monthly -->
+// PLAY
+// 10hrs <!-- weekly -->
+// Previous - 8hrs <!-- weekly -->
+// 23hrs <!-- monthly -->
+// Previous - 29hrs <!-- monthly -->
+// STUDY
+// 4hrs <!-- weekly -->
+// Previous - 7hrs <!-- weekly -->
+// 13hrs <!-- monthly -->
+// Previous - 19hrs <!-- monthly -->
+// EXERCISE
+// 4hrs <!-- weekly -->
+// Previous - 5hrs <!-- weekly -->
+// 11hrs <!-- monthly -->
+// Previous - 18hrs <!-- monthly -->
+// SOCIAL
+// 5hrs <!-- weekly -->
+// Previous - 10hrs <!-- weekly -->
+// 21hrs <!-- monthly -->
+// Previous - 23hrs <!-- monthly -->
+// SELF CARE
+// 2hrs <!-- weekly -->
+// Previous - 2hrs <!-- weekly -->
+// 7hrs <!-- monthly -->
+// Previous - 11hrs <!-- monthly -->
