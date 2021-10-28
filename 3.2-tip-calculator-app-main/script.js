c05e21155266fa5400b9d2714981e72eaeccf59a
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2021-10-28 10:30:00 +0700

    create the main program for web 3.2

--- 3.2-tip-calculator-app-main/script.js
+++ 3.2-tip-calculator-app-main/script.js
@@ -1,47 +1,16 @@
 // 
-const button = document.body.querySelector('.left > button');
+const bill = document.body.querySelector('.bill')
+const tipButton = document.body.querySelectorAll(".tip-button")
+const tipCustom = document.body.querySelector(".tip-custom")
+const numPeople = document.body.querySelector(".num-people")
+const resetButton = document.body.querySelector('.left button')
 
-while(true) {
-    calculateTip()
-
-    button.addEventListener('click', () => {
-        resetCalculate()
-    })
-
-}
-
-
-// 
-function calculateTip() {
-    let billValue = document.body.querySelector('.bill input').value;
-    let tipPercentValue = document.body.querySelector('.tip-percent input').value;
-    let numPeople = document.body.querySelector('.num-people input').value;
-
-
-    let tipAmountValue = billValue * tipPercentValue / 100 / numPeople;
-    let totalValue = +billValue + +tipAmountValue ;
-    
-    let label = 'checking the value of tipAmountValue and totalMoneyValue'
-    console.group(label);
-    console.info(tipAmountValue);
-    console.info(totalValue);
-    console.groupEnd(label)
-
-    setResult(tipAmountValue, totalValue);
-}
-
-function resetCalculate() {
-    document.body.querySelector('.bill input').value = 0;
-    document.body.querySelector('.tip-percent input').value = 0;
-    document.body.querySelector('.num-people input').value = 0;
-}
+bill.addEventListener("input", setBillValue)
+tipButton.forEach(btn => {
+    btn.addEventListener("click", setTipButtonValue)
+})
+tipCustom.addEventListener('input', setCustomValue)
+numPeople.addEventListener("input", setPeopleValue)
+resetButton.addEventListener('click', reset)
 
 // 
-const tipAmount = document.body.querySelector('.tip-amount .result');
-const totalMoney = document.body.querySelector('.total .result')
-
-function setResult(a, b) {
-    tipAmount.innerHTML = a;
-    totalMoney.innerHTML = b;
-    
-}
