3b779946b9cef84cbee8b6cb0e9c6896c3d69e73
Nguyen Thanh Tung <tungntfx08789@funix.edu.vn>
2021-11-04 09:08:29 +0700

    trying to rebuild it but wait until learn about await and async in js

--- 3.1-time-tracking-dashboard-main/script.js
+++ 3.1-time-tracking-dashboard-main/script.js
@@ -1,49 +1,34 @@
-// const timeFrames = document.body.querySelector('.report-list')
-// fetch('data.json')
-// 	.then(res => res.json())
-// 	.then(data => {
-
-// 		// main program
-// 		timeFrames.addEventListener('click', e => {
-// 			if (e.target.localName == 'a') {
-// 				showStats(data, e.target.classList[0])
-// 			}
-// 		})
-
-// 		// default stats to show
-// 		showStats(data, 'weekly')
-// 	})
-
-
-// // 
-// const sides = document.body.querySelectorAll(".side")
-	
-// function showStats(data, timeFrame) {
-// 	let context
-// 	if (timeFrame == 'monthly') context = 'Month'
-// 	if (timeFrame == 'weekly') context = 'Week'
-// 	if (timeFrame == 'daily') context = 'Day'
-
-// 	for (let card in sides) {
-// 		const cardCurrentTime = sides[card].querySelector('.current')
-// 		const cardPreviousTime = sides[card].querySelector('.previous')
-// 		cardCurrentTime.textContent = data[card].timeframes[timeFrame].current + 'hrs'
-// 		cardPreviousTime.textContent = `Last ${context} - ${data[card].timeframes[timeFrame].previous}hrs`
-// 	}
-// }
-
-
-const reportList = document.body.querySelectorAll('.report-list')
-fetch("data.json")
-	.then(respond => respond.json())
+const timeFrames = document.body.querySelector('.report-list')
+fetch('data.json')
+	.then(res => res.json())
 	.then(data => {
-		// main program 
+
+		// main program
+		timeFrames.addEventListener('click', e => {
+			if (e.target.localName == 'a') {
+				showStats(data, e.target.classList[0])
+			}
+		})
+
+		// default stats to show
+		showStats(data, 'weekly')
 	})
 
 
-// main program (1)
+// 
+const sides = document.body.querySelectorAll(".side")
+	
+function showStats(data, timeFrame) {
+	let context
+	if (timeFrame == 'monthly') context = 'Month'
+	if (timeFrame == 'weekly') context = 'Week'
+	if (timeFrame == 'daily') context = 'Day'
 
-
-// default values 
-showStats(data, weekly)
+	for (let card in sides) {
+		const cardCurrentTime = sides[card].querySelector('.current')
+		const cardPreviousTime = sides[card].querySelector('.previous')
+		cardCurrentTime.textContent = data[card].timeframes[timeFrame].current + 'hrs'
+		cardPreviousTime.textContent = `Last ${context} - ${data[card].timeframes[timeFrame].previous}hrs`
+	}
+}
 
